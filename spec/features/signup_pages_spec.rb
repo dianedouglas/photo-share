1db2fa63c44707c70bafa19da77b057ed585110a
Diane Douglas <diane.douglas1@gmail.com>
2014-09-16 11:47:48 -0700

    Add signup error tests.

--- spec/features/signup_pages_spec.rb
+++ spec/features/signup_pages_spec.rb
@@ -9,4 +9,27 @@ describe "signing up" do
     click_button 'Sign Up'
     expect(page).to have_content 'Welcome'
   end
+
+  it "show an error with a blank password field." do
+    visit 'signup'
+    fill_in 'Email', :with => 'user@example.com'
+    fill_in 'Password confirmation', :with => 'password'
+    click_button 'Sign Up'
+    expect(page).to have_content 'errors'
+  end
+
+  it "show an error with a blank password and password confirmation field." do
+    visit 'signup'
+    fill_in 'Email', :with => 'user@example.com'
+    click_button 'Sign Up'
+    expect(page).to have_content 'errors'
+  end
+
+  it "show an error with a blank email field, but correct password and password confirmation field." do
+    visit 'signup'
+    fill_in 'Password', :with => 'password'
+    fill_in 'Password confirmation', :with => 'password'
+    click_button 'Sign Up'
+    expect(page).to have_content 'errors'
+  end
 end
