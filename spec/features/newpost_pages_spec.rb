2baea4a16b8040e11398d5176e2be66c71472be4
Diane Douglas <diane.douglas1@gmail.com>
2014-09-30 10:58:29 -0700

    Add test to make sure both New Post links lead to the New Post page.

--- spec/features/newpost_pages_spec.rb
+++ spec/features/newpost_pages_spec.rb
@@ -15,4 +15,17 @@ describe "new post form page" do
     click_button 'Post'
     expect(page.find('img')['src']).to have_content 'rat_flute.gif'
   end
+
+  it "should allow a user to post a photo either from the home page or their profile page, if signed in." do
+    visit 'login'
+    user = User.create(:email => 'user@example.com', :password => 'password', :password_confirmation => 'password')
+    fill_in 'Email', :with => 'user@example.com'
+    fill_in 'Password', :with => 'password'
+    click_button 'Log In'
+    click_link 'Post a Photo!'
+    expect(page).to have_content "New Post"
+    click_link 'Pictures of You'
+    click_link 'Post a Photo!'
+    expect(page).to have_content "New Post"
+  end
 end
